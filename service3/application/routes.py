26c0107d52b5970b940117b2f07d1078f22a89e4
Your Name <you@example.com>
2020-03-05 01:34:08 +0000

    service 3 now prints names by genderID

--- service3/application/routes.py
+++ service3/application/routes.py
@@ -6,13 +6,13 @@ from sqlalchemy import func, select
 @app.route('/name', methods=['GET', 'POST'])
 def name():
 
-    gender_genderid = 3
+    gender_genderid = 1
 
     if gender_genderid == 3:
-        name = Name.query.order_by(func.random()).limit(1)
-#    else:
-        #name= Name.query.filter_by(id=gender_genderid).order_by(func.random())).limit(1)
+        name = Name.query.order_by(func.random()).limit(1).first()
+    else:
+        name= Name.query.filter_by(genderno=gender_genderid).order_by(func.random()).limit(1).first()
  #       name= Name.query.order_by(func.random()).filter_by(id=gender_genderid).first()
 
-    return str(name)
+    return str(name.value)
 
